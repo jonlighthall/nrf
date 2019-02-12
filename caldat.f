3aceb42c6ebc3e5091eaf634e041e3b0e3a127c4
Jon Lighthall <jon.lighthall@gmail.com>
2019-02-12 17:48:30 -0600

    format files after cut-and-paste

--- caldat.f
+++ caldat.f
@@ -1,30 +1,29 @@
-SUBROUTINE caldat(julian,mm,id,iyyy)
-INTEGER id,iyyy,julian,mm,IGREG
-PARAMETER (IGREG=2299161)
-Inverse of the function julday given above. Here julian is input as a Julian Day Number,
-and the routine outputs mm,id, and iyyy as the month, day, and year on which the specied
-Julian Day started at noon.
-INTEGER ja,jalpha,jb,jc,jd,je
-if(julian.ge.IGREG)then Cross-over to Gregorian Calendar produces
-jalpha=int(((julian-1867216)-0.25)/36524.25) this correction.
-ja=julian+1+jalpha-int(0.25*jalpha)
-else if(julian.lt.0)then Make day number positive by adding integer
-number of Julian centuries, then
-subtract them o at the end.
-ja=julian+36525*(1-julian/36525)
-else
-ja=julian
-endif
-jb=ja+1524
-jc=int(6680.+((jb-2439870)-122.1)/365.25)
-jd=365*jc+int(0.25*jc)
-je=int((jb-jd)/30.6001)
-id=jb-jd-int(30.6001*je)
-mm=je-1
-if(mm.gt.12)mm=mm-12
-iyyy=jc-4715
-if(mm.gt.2)iyyy=iyyy-1
-if(iyyy.le.0)iyyy=iyyy-1
-if(julian.lt.0)iyyy=iyyy-100*(1-julian/36525)
-return
-END
+      SUBROUTINE caldat(julian,mm,id,iyyy)
+      INTEGER id,iyyy,julian,mm,IGREG
+      PARAMETER (IGREG=2299161)
+c     Inverse of the function julday given above. Here julian is input
+c     as a Julian Day Number, and the routine outputs mm,id, and iyyy as
+c     the month, day, and year on which the specified Julian Day started
+c     at noon. 
+      INTEGER ja,jalpha,jb,jc,jd,je
+      if(julian.ge.IGREG)then   ! Cross-over to Gregorian Calendar produces this correction.
+         jalpha=int(((julian-1867216)-0.25)/36524.25) 
+         ja=julian+1+jalpha-int(0.25*jalpha)
+      else if(julian.lt.0)then  ! Make day number positive by adding integer number of Julian centuries, then subtract them off at the end.
+         ja=julian+36525*(1-julian/36525)
+      else
+         ja=julian
+      endif
+      jb=ja+1524
+      jc=int(6680.+((jb-2439870)-122.1)/365.25)
+      jd=365*jc+int(0.25*jc)
+      je=int((jb-jd)/30.6001)
+      id=jb-jd-int(30.6001*je)
+      mm=je-1
+      if(mm.gt.12)mm=mm-12
+      iyyy=jc-4715
+      if(mm.gt.2)iyyy=iyyy-1
+      if(iyyy.le.0)iyyy=iyyy-1
+      if(julian.lt.0)iyyy=iyyy-100*(1-julian/36525)
+      return
+      END
--- ludcmp.f
+++ ludcmp.f
@@ -1,50 +1,52 @@
-SUBROUTINE ludcmp(a,n,np,indx,d)
-INTEGER n,np,indx(n),NMAX
-REAL d,a(np,np),TINY
-PARAMETER (NMAX=500,TINY=1.0e-20) Largest expected n, and a small number.
-Given a matrix a(1:n,1:n), with physical dimension np by np, this routine replaces it by
-the LU decomposition of a rowwise permutation of itself. a and n are input. a is output,
-arranged as in equation (2.3.14) above; indx(1:n) is an output vector that records the
-row permutation eected by the partial pivoting; d is output as 1 depending on whether
-the number of row interchanges was even or odd, respectively. This routine is used in
-combination with lubksb to solve linear equations or invert a matrix.
-INTEGER i,imax,j,k
-REAL aamax,dum,sum,vv(NMAX) vv stores the implicit scaling of each row.
-d=1. No row interchanges yet.
-do 12 i=1,n Loop over rows to get the implicit scaling informaaamax=
-0. tion.
-do 11 j=1,n
-if (abs(a(i,j)).gt.aamax) aamax=abs(a(i,j))
-enddo 11
-if (aamax.eq.0.) pause 'singular matrix in ludcmp' No nonzero largest element.
-vv(i)=1./aamax Save the scaling.
-enddo 12
-do 19 j=1,n This is the loop over columns of Crout's method.
-do 14 i=1,j-1 This is equation (2.3.12) except for i = j.
-sum=a(i,j)
-do 13 k=1,i-1
-sum=sum-a(i,k)*a(k,j)
-enddo 13
-a(i,j)=sum
-enddo 14
-aamax=0. Initialize for the search for largest pivot element.
-do 16 i=j,n This is i = j of equation (2.3.12) and i = j+1: ::N
-sum=a(i,j) of equation (2.3.13).
-do 15 k=1,j-1
-sum=sum-a(i,k)*a(k,j)
-enddo 15
-a(i,j)=sum
-dum=vv(i)*abs(sum) Figure of merit for the pivot.
-if (dum.ge.aamax) then Is it better than the best so far?
-imax=i
-aamax=dum
-endif
-enddo 16
-if (j.ne.imax)then Do we need to interchange rows?
-do 17 k=1,n Yes, do so...
-dum=a(imax,k)
-a(imax,k)=a(j,k)
-a(j,k)=dum
+      SUBROUTINE ludcmp(a,n,np,indx,d)
+      INTEGER n,np,indx(n),NMAX
+      REAL d,a(np,np),TINY
+      PARAMETER (NMAX=500,TINY=1.0e-20) ! Largest expected n, and a small number.
+c     Given a matrix a(1:n,1:n), with physical dimension np by np, this
+c     routine replaces it by the LU decomposition of a rowwise
+c     permutation of itself. a and n are input. a is output, arranged as
+c     in equation (2.3.14) above; indx(1:n) is an output vector that
+c     records the row permutation effected by the partial pivoting; d is
+c     output as +/-1 depending on whether the number of row interchanges
+c     was even or odd, respectively. This routine is used in combination
+c     with lubksb to solve linear equations or invert a matrix. 
+      INTEGER i,imax,j,k
+      REAL aamax,dum,sum,vv(NMAX) ! vv stores the implicit scaling of each row.
+      d=1.                      ! No row interchanges yet.
+      do 12 i=1,n               ! Loop over rows to get the implicit scaling informtion. 
+      aaamax=0. 
+      do 11 j=1,n
+         if (abs(a(i,j)).gt.aamax) aamax=abs(a(i,j))
+      enddo 11
+      if (aamax.eq.0.) pause 'singular matrix in ludcmp' ! No nonzero largest element.
+      vv(i)=1./aamax            ! Save the scaling.
+      enddo 12
+      do 19 j=1,n               ! This is the loop over columns of Crout's method.
+         do 14 i=1,j-1          ! This is equation (2.3.12) except for i = j.
+            sum=a(i,j)
+            do 13 k=1,i-1
+               sum=sum-a(i,k)*a(k,j)
+            enddo 13
+            a(i,j)=sum
+         enddo 14
+         aamax=0.               ! Initialize for the search for largest pivot element.
+         do 16 i=j,n            ! This is i = j of equation (2.3.12) and i = j+1...N of equation (2.3.13).
+            sum=a(i,j) 
+            do 15 k=1,j-1
+               sum=sum-a(i,k)*a(k,j)
+            enddo 15
+            a(i,j)=sum
+            dum=vv(i)*abs(sum)  ! Figure of merit for the pivot.
+            if (dum.ge.aamax) then ! Is it better than the best so far?
+               imax=i
+               aamax=dum
+            endif
+         enddo 16
+         if (j.ne.imax)then     ! Do we need to interchange rows?
+            do 17 k=1,n Yes, do so...
+               dum=a(imax,k)
+               a(imax,k)=a(j,k)
+               a(j,k)=dum
 enddo 17
 d=-d ...and change the parity of d.
 vv(imax)=vv(j) Also interchange the scale factor.
