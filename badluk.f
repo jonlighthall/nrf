e59a83d17787b83cdda7f302347e6aafd1a60cdd
Jon Lighthall <jon.lighthall@gmail.com>
2019-02-12 17:48:30 -0600

    format files after cut-and-paste

--- badluk.f
+++ badluk.f
@@ -1,61 +1,52 @@
-PROGRAM badluk
-INTEGER ic,icon,idwk,ifrac,im,iybeg,iyend,iyyy,jd,jday,n,
-* julday
-REAL TIMZON,frac
-PARAMETER (TIMZON=-5./24.) Time zone −5 is Eastern Standard Time.
-DATA iybeg,iyend /1900,2000/ The range of dates to be searched.
-C USES flmoon,julday
-write (*,'(1x,a,i5,a,i5)') 'Full moons on Friday the 13th from',
-* iybeg,' to',iyend
-do 12 iyyy=iybeg,iyend Loop over each year,
-do 11 im=1,12 and each month.
-jday=julday(im,13,iyyy) Is the 13th a Friday?
-idwk=mod(jday+1,7)
-if(idwk.eq.5) then
-n=12.37*(iyyy-1900+(im-0.5)/12.)
-This value n is a rst approximation to how many full moons have occurred
-since 1900. We will feed it into the phase routine and adjust it up or down until
-1.1 Program Organization and Control Structures 15
-Sample page from NUMERICAL RECIPES IN FORTRAN 77: THE ART OF SCIENTIFIC COMPUTING (ISBN 0-521-43064-X)
-Copyright (C) 1986-1992 by Cambridge University Press.
-Programs Copyright (C) 1986-1992 by Numerical Recipes Software.
-Permission is granted for internet users to make one paper copy for their own personal use. Further reproduction, or any copying of machinereadable
-files (including this one) to any server
-computer, is strictly prohibited. To order Numerical Recipes books,
-diskettes, or CDROMs
-visit website http://www.nr.com or call 1-800-872-7423 (North America only),
-or send email to trade@cup.cam.ac.uk (outside North America).
-we determine that our desired 13th was or was not a full moon. The variable
-icon signals the direction of adjustment.
-icon=0
-1 call flmoon(n,2,jd,frac) Get date of full moon n.
-ifrac=nint(24.*(frac+TIMZON)) Convert to hours in correct time zone.
-if(ifrac.lt.0)then Convert from Julian Days beginning at noon
-to civil jd=jd-1 days beginning at midnight.
-ifrac=ifrac+24
-endif
-if(ifrac.gt.12)then
-jd=jd+1
-ifrac=ifrac-12
-else
-ifrac=ifrac+12
-endif
-if(jd.eq.jday)then Did we hit our target day?
-write (*,'(/1x,i2,a,i2,a,i4)') im,'/',13,'/',iyyy
-write (*,'(1x,a,i2,a)') 'Full moon ',ifrac,
-* ' hrs after midnight (EST).'
-Don't worry if you are unfamiliar with FORTRAN's esoteric input/output
-statements; very few programs in this book do any input/output.
-goto 2 Part of the break-structure, case of a match.
-else Didn't hit it.
-ic=isign(1,jday-jd)
-if(ic.eq.-icon) goto 2 Another break, case of no match.
-icon=ic
-n=n+ic
-endif
-goto 1
-2 continue
-endif
-enddo 11
-enddo 12
-END
+      PROGRAM badluk
+      INTEGER ic,icon,idwk,ifrac,im,iybeg,iyend,iyyy,jd,jday,n,
+     *     julday
+      REAL TIMZON,frac
+      PARAMETER (TIMZON=-5./24.) ! Time zone −5 is Eastern Standard Time.
+      DATA iybeg,iyend /1900,2000/ ! The range of dates to be searched.
+C     USES flmoon,julday
+      write (*,'(1x,a,i5,a,i5)') 'Full moons on Friday the 13th from',
+     *     iybeg,' to',iyend
+      do 12 iyyy=iybeg,iyend ! Loop over each year,
+         do 11 im=1,12 ! and each month.
+            jday=julday(im,13,iyyy) ! Is the 13th a Friday?
+            idwk=mod(jday+1,7)
+            if(idwk.eq.5) then
+               n=12.37*(iyyy-1900+(im-0.5)/12.)
+c     This value n is a rst approximation to how many full moons have
+c     occurred since 1900. We will feed it into the phase routine and
+c     adjust it up or down until we determine that our desired 13th was
+c     or was not a full moon. The variable icon signals the direction of
+c     adjustment. 
+               icon=0
+ 1             call flmoon(n,2,jd,frac) ! Get date of full moon n.
+               ifrac=nint(24.*(frac+TIMZON)) ! Convert to hours in correct time zone.
+               if(ifrac.lt.0)then ! Convert from Julian Days beginning at noon to civil jd=jd-1 days beginning at midnight.
+                  ifrac=ifrac+24
+               endif
+               if(ifrac.gt.12)then
+                  jd=jd+1
+                  ifrac=ifrac-12
+               else
+                  ifrac=ifrac+12
+               endif
+               if(jd.eq.jday)then ! Did we hit our target day?
+                  write (*,'(/1x,i2,a,i2,a,i4)') im,'/',13,'/',iyyy
+                  write (*,'(1x,a,i2,a)') 'Full moon ',ifrac,
+     *                 ' hrs after midnight (EST).'
+c     Don't worry if you are unfamiliar with FORTRAN's esoteric input
+c     /output statements; very few programs in this book do any input
+c     /output. 
+                  goto 2        ! Part of the break-structure, case of a match.
+               else             ! Didn't hit it.
+                  ic=isign(1,jday-jd)
+                  if(ic.eq.-icon) goto 2 ! Another break, case of no match.
+                  icon=ic
+                  n=n+ic
+               endif
+               goto 1
+ 2             continue
+            endif
+         enddo 11
+      enddo 12
+      END
--- flmoon.f
+++ flmoon.f
@@ -1,46 +1,35 @@
-SUBROUTINE flmoon(n,nph,jd,frac)
-INTEGER jd,n,nph
-REAL frac,RAD
-PARAMETER (RAD=3.14159265/180.)
-Our programs begin with an introductory comment summarizing their purpose and explaining
-their calling sequence. This routine calculates the phases of the moon. Given an integer
-n and a code nph for the phase desired (nph = 0 for new moon, 1 for rst quarter, 2 for
-full, 3 for last quarter), the routine returns the Julian Day Number jd, and the fractional
-part of a day frac to be added to it, of the nth such phase since January, 1900. Greenwich
-Mean Time is assumed.
-INTEGER i
-REAL am,as,c,t,t2,xtra
-c=n+nph/4. This is how we comment an individual line.
-t=c/1236.85
-t2=t**2
-1
-2 Chapter 1. Preliminaries
-Sample page from NUMERICAL RECIPES IN FORTRAN 77: THE ART OF SCIENTIFIC COMPUTING (ISBN 0-521-43064-X)
-Copyright (C) 1986-1992 by Cambridge University Press.
-Programs Copyright (C) 1986-1992 by Numerical Recipes Software.
-Permission is granted for internet users to make one paper copy for their own personal use. Further reproduction, or any copying of machinereadable
-files (including this one) to any server
-computer, is strictly prohibited. To order Numerical Recipes books,
-diskettes, or CDROMs
-visit website http://www.nr.com or call 1-800-872-7423 (North America only),
-or send email to trade@cup.cam.ac.uk (outside North America).
-as=359.2242+29.105356*c You aren't really intended to understand this alam=
-306.0253+385.816918*c+0.010730*t2 gorithm, but it does work!
-jd=2415020+28*n+7*nph
-xtra=0.75933+1.53058868*c+(1.178e-4-1.55e-7*t)*t2
-if(nph.eq.0.or.nph.eq.2)then
-xtra=xtra+(0.1734-3.93e-4*t)*sin(RAD*as)-0.4068*sin(RAD*am)
-else if(nph.eq.1.or.nph.eq.3)then
-xtra=xtra+(0.1721-4.e-4*t)*sin(RAD*as)-0.6280*sin(RAD*am)
-else
-pause 'nph is unknown in flmoon' This is how we will indicate error conditions.
-endif
-if(xtra.ge.0.)then
-i=int(xtra)
-else
-i=int(xtra-1.)
-endif
-jd=jd+i
-frac=xtra-i
-return
-END
+      SUBROUTINE flmoon(n,nph,jd,frac)
+      INTEGER jd,n,nph
+      REAL frac,RAD
+      PARAMETER (RAD=3.14159265/180.)
+c     This routine calculates the phases of the moon. Given an integer n
+c     and a code nph for the phase desired (nph = 0 for new moon, 1 for
+c     first quarter, 2 for full, 3 for last quarter), the routine
+c     returns the Julian Day Number jd, and the fractional part of a day
+c     frac to be added to it, of the nth such phase since January, 1900.
+c     Greenwich Mean Time is assumed. 
+      INTEGER i
+      REAL am,as,c,t,t2,xtra
+      c=n+nph/4.                ! This is how we comment an individual line.
+      t=c/1236.85
+      t2=t**2
+      as=359.2242+29.105356*c   ! You aren't really intended to understand this algorithm, but it does work!
+      am=306.0253+385.816918*c+0.010730*t2 
+      jd=2415020+28*n+7*nph
+      xtra=0.75933+1.53058868*c+(1.178e-4-1.55e-7*t)*t2
+      if(nph.eq.0.or.nph.eq.2)then
+         xtra=xtra+(0.1734-3.93e-4*t)*sin(RAD*as)-0.4068*sin(RAD*am)
+      else if(nph.eq.1.or.nph.eq.3)then
+         xtra=xtra+(0.1721-4.e-4*t)*sin(RAD*as)-0.6280*sin(RAD*am)
+      else
+         pause 'nph is unknown in flmoon' ! This is how we will indicate error conditions.
+      endif
+      if(xtra.ge.0.)then
+         i=int(xtra)
+      else
+         i=int(xtra-1.)
+      endif
+      jd=jd+i
+      frac=xtra-i
+      return
+      END
