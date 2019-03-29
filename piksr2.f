b86aba3e9ca32437781ea58dfb04029c01517033
Jon Lighthall <jon.lighthall@gmail.com>
2019-03-29 13:24:43 -0500

    format as written

--- piksr2.f
+++ piksr2.f
@@ -1,21 +1,22 @@
-SUBROUTINE piksr2(n,arr,brr)
-INTEGER n
-REAL arr(n),brr(n)
-Sorts an array arr(1:n) into ascending numerical order, by straight insertion, while making
-the corresponding rearrangement of the array brr(1:n).
-INTEGER i,j
-REAL a,b
-do 12 j=2,n Pick out each element in turn.
-a=arr(j)
-b=brr(j)
-do 11 i=j-1,1,-1 Look for the place to insert it.
-if(arr(i).le.a)goto 10
-arr(i+1)=arr(i)
-brr(i+1)=brr(i)
-enddo 11
-i=0
-10 arr(i+1)=a Insert it.
-brr(i+1)=b
-enddo 12
-return
-END
+      SUBROUTINE piksr2(n,arr,brr)
+      INTEGER n
+      REAL arr(n),brr(n)
+c     Sorts an array arr(1:n) into ascending numerical order, by
+c     straight insertion, while making the corresponding rearrangement
+c     of the array brr(1:n).
+      INTEGER i,j
+      REAL a,b
+      do 12 j=2,n               ! Pick out each element in turn.
+         a=arr(j)
+         b=brr(j)
+         do 11 i=j-1,1,-1       ! Look for the place to insert it.
+            if(arr(i).le.a)goto 10
+            arr(i+1)=arr(i)
+            brr(i+1)=brr(i)
+         enddo 11
+         i=0
+ 10      arr(i+1)=a             ! Insert it.
+         brr(i+1)=b
+      enddo 12
+      return
+      END
