ae715d4349b4763fdf8da8cdbe7fe4c7fdc31d2c
Jon Lighthall <jon.lighthall@gmail.com>
2019-03-30 11:11:01 -0500

    update formatting

--- piksr2.dem.f
+++ piksr2.dem.f
@@ -7,28 +7,28 @@ C	Driver for routine PIKSR2
 	CLOSE(UNIT)
 C	Generate B-array
 	DO 11 I=1,100
-		B(I)=I
-11	CONTINUE
+	   B(I)=I
+ 11	CONTINUE
 C	Sort A and mix B
 	CALL PIKSR2(100,A,B)
 	WRITE(*,*) 'After sorting A and mixing B, array A is:'
 	DO 12 I=1,10
-		WRITE(*,'(1X,10F6.2)') (A(10*(I-1)+J), J=1,10)
-12	CONTINUE
+	   WRITE(*,'(1X,10F6.2)') (A(10*(I-1)+J),J=1,10)
+ 12	CONTINUE
 	WRITE(*,*) '...and array B is:'
 	DO 13 I=1,10
-		WRITE(*,'(1X,10F6.2)') (B(10*(I-1)+J), J=1,10)
-13	CONTINUE
+	   WRITE(*,'(1X,10F6.2)') (B(10*(I-1)+J), J=1,10)
+ 13	CONTINUE
 	WRITE(*,*) 'press RETURN to continue...'
 	READ(*,*)
 C	Sort B and mix A
 	CALL PIKSR2(100,B,A)
 	WRITE(*,*) 'After sorting B and mixing A, array A is:'
 	DO 14 I=1,10
-		WRITE(*,'(1X,10F6.2)') (A(10*(I-1)+J), J=1,10)
-14	CONTINUE
+	   WRITE(*,'(1X,10F6.2)') (A(10*(I-1)+J), J=1,10)
+ 14	CONTINUE
 	WRITE(*,*) '...and array B is:'
 	DO 15 I=1,10
-		WRITE(*,'(1X,10F6.2)') (B(10*(I-1)+J), J=1,10)
-15	CONTINUE
+	   WRITE(*,'(1X,10F6.2)') (B(10*(I-1)+J), J=1,10)
+ 15	CONTINUE
 	END
