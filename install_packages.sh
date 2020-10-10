de4a9dbc7a1bde05e117433b9f5b2d9fd72cee44
Jon Lighthall <jon.lighthall@gmail.com>
2020-10-09 21:23:14 -0500

    refine install

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-sudo apt update && sudo apt -y upgrade
+
+# update and upgrade
+sudo apt update
+sudo apt upgrade -y
+
+# install dependancies
 sudo apt -y install git
 sudo apt -y install make
 sudo apt -y install gfortran
+
+# re-check and cleanup
+sudo apt upgrade -y --fix-missing
+sudo apt autoremove -y
