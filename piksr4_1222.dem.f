5ea24d2540e796f9548d60aa116250f899318dd5
Jon Lighthall <jon.lighthall@gmail.com>
2022-06-16 16:28:07 -0500

    FIX integer write and add variable format

--- piksr4_1222.dem.f
+++ piksr4_1222.dem.f
@@ -13,7 +13,7 @@ C	Driver for routine PIKSR4_1222
 	INTEGER, PARAMETER :: X=6, Y=2, DIV=2
 	DIMENSION A(X),B(X,Y),C(X,Y)
 	INTEGER, PARAMETER :: UNIT=1 ! unit 5 reserved for keyboard
-	CHARACTER(LEN = 256) :: FMT
+	CHARACTER(LEN = 256) :: FMT,iFMT,sFMT
 	CHARACTER(LEN = 3) :: D(X,Y)
 	OPEN(UNIT,FILE='tarray.dat',STATUS='OLD')
 	READ(UNIT,*) (A(I),I=1,X)
@@ -23,26 +23,29 @@ C	Generate B and C arrays
 	   DO J=1,Y
 	      B(I,J)=I-1+J-1
 	      C(I,J)=(A(I)+B(I,J))/2+J-1
-	      WRITE(D(I,J),'(F3.0)')C(I,J)
+	      WRITE(D(I,J),'(I3)')C(I,J)
 	   ENDDO
  11	CONTINUE
+c	Format printing
+	write(FMT,'("(1x,",i0,"f7.2)")') DIV
+	write(iFMT,'("(1x,",i0,"i3)")') DIV
+	write(sFMT,'("(1x,",i0,"A3)")') DIV
 C	Print original arrays
-	WRITE(*,*) 'Original arrys:'
-	WRITE(FMT,'(A)')'(1X,10F7.2)'
+	WRITE(*,*) 'Before sorting, array A is:'
 	DO 12 I=1,X/DIV
 	   WRITE(*,FMT) (A(DIV*(I-1)+J), J=1,DIV)
  12	CONTINUE
 	WRITE(*,*) '...and array B is:'
 	DO 13 I=1,X/DIV
-	   WRITE(*,FMT) (B(DIV*(I-1)+J,:), J=1,DIV)
+	   WRITE(*,iFMT) (B(DIV*(I-1)+J,:), J=1,DIV)
  13	CONTINUE
 	WRITE(*,*) '...and array C is:'
 	DO 16 I=1,X/DIV
-	   WRITE(*,FMT) (C(DIV*(I-1)+J,:), J=1,DIV)
+	   WRITE(*,iFMT) (C(DIV*(I-1)+J,:), J=1,DIV)
  16	CONTINUE
 	WRITE(*,*) '...and array D is:'
 	DO I=1,X/DIV
-	   WRITE(*,'(3X,10(A,4X))') (D(DIV*(I-1)+J,:), J=1,DIV)
+	   WRITE(*,sFMT) (D(DIV*(I-1)+J,:), J=1,DIV)
 	ENDDO
 	WRITE(*,*) 'press RETURN to continue...'
 	READ(*,*)
@@ -54,14 +57,14 @@ C	Sort B and mix A,C
  14	CONTINUE
 	WRITE(*,*) '...and array B is:'
 	DO 15 I=1,X/DIV
-	   WRITE(*,FMT) (B(DIV*(I-1)+J,:), J=1,DIV)
+	   WRITE(*,iFMT) (B(DIV*(I-1)+J,:), J=1,DIV)
  15	CONTINUE
 	WRITE(*,*) '...and array C is:'
 	DO 17 I=1,X/DIV
-	   WRITE(*,FMT) (C(DIV*(I-1)+J,:), J=1,DIV)
+	   WRITE(*,iFMT) (C(DIV*(I-1)+J,:), J=1,DIV)
  17	CONTINUE
 	WRITE(*,*) '...and array D is:'
 	DO I=1,X/DIV
-	   WRITE(*,'(3X,10(A,4X))') (D(DIV*(I-1)+J,:), J=1,DIV)
+	   WRITE(*,sFMT) (D(DIV*(I-1)+J,:), J=1,DIV)
 	ENDDO
 	END
