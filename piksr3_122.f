ce21dcee60d083726d65c4b9017d5490f6d2cafb
Jon Lighthall <jon.lighthall@gmail.com>
2022-07-12 18:21:30 -0500

    remove labeled do loops

--- piksr3_122.f
+++ piksr3_122.f
@@ -7,20 +7,20 @@ c     of the 2D arrays brr(1:n,1:bn) and crr(1:n,1:cn).
       INTEGER i,j
       REAL a
       real b(bn),c(cn)
-      do 12 j=2,n               ! Pick out each element in turn.
+      do j=2,n                  ! Pick out each element in turn.
          a=arr(j)
          b=brr(j,:)
          c=crr(j, :)
-         do 11 i=j-1,1,-1       ! Look for the place to insert it.
+         do i=j-1,1,-1          ! Look for the place to insert it.
             if(arr(i).le.a)goto 10
             arr(i+1)=arr(i)
             brr(i+1,:)=brr(i,:)
             crr(i+1,:)=crr(i,:)
- 11      enddo 
+         enddo
          i=0
  10      arr(i+1)=a             ! Insert it.
          brr(i+1,:)=b
          crr(i+1,:)=c
- 12   enddo 
+      enddo
       return
       END
